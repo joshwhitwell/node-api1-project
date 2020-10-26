cd9fde9a53970ee3302b981393ad0a2ad6cab227
Joshua Whitwell <josh.whitwell@gmail.com>
2020-10-26 16:54:45 -0400

    added post user and put user endpoints

--- index.js
+++ index.js
@@ -25,8 +25,39 @@ server.get('/users', (req, res) => {
 })
 
 // [ POST ] endpoints
+server.post('/users', (req, res) => {
+    const { name, bio } = req.body
+    try {
+        if(!name || !bio) {
+            res.status(400).json({ message: 'Request must include a name and a bio'})
+        } else {
+            const newUser = { id: generate(), name, bio }
+            users.push(newUser)
+            res.status(201).json({ message: 'New user created', data: users })
+        }
+    } catch(error) {
+        res.status(500).json({ message: 'Failure adding user', error })
+    }
+})
 
 // [ PUT ] endpoints
+server.put('/users/:id', (req, res) => {
+    try {
+        const { id } = req.params
+        const { name, bio } = req.body
+        const index = users.findIndex(user => user.id === id)
+        if(index !== -1 && name && bio) {
+            users[index] = { id, name, bio }
+            res.status(200).json({ message: 'User has been update', data: users })
+        } else if (index === -1) {
+            res.status(404).json({ message: `Could not find user with id ${id}` })
+        } else if (!name || !bio) {
+            res.status(400).json({ message: 'Request must include a name and a bio'})
+        }
+    } catch(error) {
+        res.status(500).json({ message: 'Failure updating user', error })
+    }
+})
 
 // [ DELETE ] endpoints
 
